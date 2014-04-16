3355fa754b0ceb4065a36712e5a1ca13b8ce8f70
Jonas Mueller <muellerj@me.com>
2014-04-16 08:30:24 +0200

    Move location detail processor into its own file

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require_relative "lib/location_detail_processor"
 
 def location_detail_template
   template = <<-EOT.gsub(/^ {4}/, '')
@@ -23,29 +24,6 @@ def location_detail_template
   template
 end
 
-class LocationDetailProcessor
-  def self.process_location_detail(content)
-    fileparts = content.gsub("\r", "").split(/^---$/)
-
-    fail "Required file section not found!" if fileparts[1].nil?
-
-    eval fileparts[1].lines.to_a[2..-2].join
-
-    total = instance_variables.reduce(0) { |sum, v| sum += instance_variable_get(v) }
-
-    outputtable = "\n\nPosten | Wert\n"
-    outputtable << " --- | ---:\n"
-    instance_variables.each do |var|
-      outputtable << "#{var.to_s.tr("@", "").capitalize} | #{instance_variable_get(var).to_s}\n"
-    end
-    outputtable << "**TOTAL** | **#{total}**\n\n"
-
-    newfile = fileparts
-    newfile[2] = outputtable
-
-    newfile.join("---")
-  end
-end
 
 def render_form(content)
   erb <<-EOS
